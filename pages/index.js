e40c312b145b576f80a7b5d7182b87fc152bb18c
JAO CHINE <47437339+Chaihelloworld@users.noreply.github.com>
2023-10-03 12:08:21 +0700

    commit update hide css

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import Highcharts from "highcharts";
-import DataJson from "./data.json";
-import ChartMain from "./chart.js";
+import ChartMain from "../component/chart";
 function MyChartComponent() {
   return (
     <>
